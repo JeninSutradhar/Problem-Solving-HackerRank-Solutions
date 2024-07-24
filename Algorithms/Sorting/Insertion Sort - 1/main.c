c0563644697cbef623f6da0943264f6bcbe8fc86
jeninsutradhar <jeninsutradhar@gmail.com>
2024-07-24 15:35:22 +0530

    updated

---
+++ Algorithms/Sorting/Insertion Sort - 1/main.c
@@ -0,0 +1,174 @@
+#include <assert.h>
+#include <ctype.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* readline();
+char* ltrim(char*);
+char* rtrim(char*);
+
+int parse_int(char*);
+
+// Function to Sort the Array of String
+int compare(const void *a, const void *b) {
+    char *strA = *(char **)a;
+    char *strB = *(char **)b;
+    
+    // Compare the length first
+    int lenA = strlen(strA);
+    int lenB = strlen(strB);
+    
+    if (lenA != lenB) {
+        return lenA - lenB;
+    }
+    
+    // If lengths are same, then compare lexicographically
+    return strcmp(strA, strB);
+}
+
+char** bigSorting(int unsorted_count, char** unsorted, int* result_count) {
+    // Sort the Array using qsort and and compare method
+    qsort(unsorted, unsorted_count, sizeof(char*), compare);
+    
+    // Set the Result_count to the size of Array
+    *result_count = unsorted_count;
+    
+    return unsorted;
+}
+
+int main()
+{
+    FILE* fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+    int n = parse_int(ltrim(rtrim(readline())));
+
+    char** unsorted = malloc(n * sizeof(char*));
+
+    for (int i = 0; i < n; i++) {
+        char* unsorted_item = readline();
+
+        *(unsorted + i) = unsorted_item;
+    }
+
+    int result_count;
+    char** result = bigSorting(n, unsorted, &result_count);
+
+    for (int i = 0; i < result_count; i++) {
+        fprintf(fptr, "%s", *(result + i));
+
+        if (i != result_count - 1) {
+            fprintf(fptr, "\n");
+        }
+    }
+
+    fprintf(fptr, "\n");
+
+    fclose(fptr);
+
+    return 0;
+}
+
+char* readline() {
+    size_t alloc_length = 1024;
+    size_t data_length = 0;
+
+    char* data = malloc(alloc_length);
+
+    while (true) {
+        char* cursor = data + data_length;
+        char* line = fgets(cursor, alloc_length - data_length, stdin);
+
+        if (!line) {
+            break;
+        }
+
+        data_length += strlen(cursor);
+
+        if (data_length < alloc_length - 1 || data[data_length - 1] == '\n') {
+            break;
+        }
+
+        alloc_length <<= 1;
+
+        data = realloc(data, alloc_length);
+
+        if (!data) {
+            data = '\0';
+
+            break;
+        }
+    }
+
+    if (data[data_length - 1] == '\n') {
+        data[data_length - 1] = '\0';
+
+        data = realloc(data, data_length);
+
+        if (!data) {
+            data = '\0';
+        }
+    } else {
+        data = realloc(data, data_length + 1);
+
+        if (!data) {
+            data = '\0';
+        } else {
+            data[data_length] = '\0';
+        }
+    }
+
+    return data;
+}
+
+char* ltrim(char* str) {
+    if (!str) {
+        return '\0';
+    }
+
+    if (!*str) {
+        return str;
+    }
+
+    while (*str != '\0' && isspace(*str)) {
+        str++;
+    }
+
+    return str;
+}
+
+char* rtrim(char* str) {
+    if (!str) {
+        return '\0';
+    }
+
+    if (!*str) {
+        return str;
+    }
+
+    char* end = str + strlen(str) - 1;
+
+    while (end >= str && isspace(*end)) {
+        end--;
+    }
+
+    *(end + 1) = '\0';
+
+    return str;
+}
+
+int parse_int(char* str) {
+    char* endptr;
+    int value = strtol(str, &endptr, 10);
+
+    if (endptr == str || *endptr != '\0') {
+        exit(EXIT_FAILURE);
+    }
+
+    return value;
+}
